109ac77008620acc7ae22c7c0c9fc138aafb9aed
Fran <montanarifranco11@hotmail.com>
2023-03-19 11:52:21 -0300

    Initial commit

---
+++ src/test/java/com/socialNetwork/SocialNetworkBackend/SocialNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialNetwork.SocialNetworkBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
